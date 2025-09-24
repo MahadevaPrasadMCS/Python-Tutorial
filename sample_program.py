93a5fa50e20b98d5bad9075162b69ff7cf4ac12b
Mahadeva Prasad M <mahadevaprasadcs23@gmail.com>
2025-09-25 01:19:06 +0530

    Delete sample_program.py

--- sample_program.py
+++
@@ -1,8 +0,0 @@
-name = "Mahadeva Prasad M" # Variable to store name
-age = 25 # Variable to store age
-print("Name:", name) # Print name
-print("Age:", age) # Print age  
-
-# we can check type of variable using type() function
-print("Type of name:", type(name)) # Print type of name 
-print("Type of age:", type(age)) # Print type of age
