0be199ac77b9aa1430db272504a3296ac82782f6
Mahadeva Prasad M <mahadevaprasadcs23@gmail.com>
2025-10-27 23:48:52 +0530

    Revisited the output statement in python

--- Python Tutorial/Input_and_output.py
+++ Python Tutorial/Input_and_output.py
@@ -1,4 +1,9 @@
-n = 6
-for i in range(1,n):
-   n -= 1
-   print(" " * i + "*" * n)
+#Output statement in python
+
+print("Hello World!") #Hello World!
+print("My name is Mahadeva Prasad M") #My name is Mahadeva Prasad M
+
+#fstring or format string
+
+name = "Prasad"
+print(f"My name is {name}") # My name is Prasad
